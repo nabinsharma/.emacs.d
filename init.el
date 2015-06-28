de804aabc1e2278601bfdd47a3d02b4ded8167c0
Nabin Sharma <nabin.s.sharma@gmail.com>
2015-06-28 18:53:39 -0400

    emacs setup updates (mostly for work)

--- init.el
+++ init.el
@@ -2,9 +2,19 @@
 ; Nabin Sharma
 ; Dec 10, 2012
 
+;;;; For backward compatibility.
+(require 'cl)
+
+;;;; Package manager.
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
 ;;;; Default directory (home).
 (setq default-directory "~")
 
+;;;; Theme.
+(load-theme 'hickey t)
 
 ;;;; Load required path and start server.
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp"))
@@ -40,7 +50,6 @@
 ;; Default indentation of 2 spaces.
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
-(setq-default c-basic-offset 2)
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
 (menu-bar-mode -1)
@@ -80,9 +89,12 @@
 (add-to-list 'auto-mode-alist '("\\.hxx$" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.idl$" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.ipp$" . c++-mode))
-;; Google C style
-(require 'google-c-style)
-(add-hook 'c-mode-common-hook 'google-set-c-style)
+;; Google C style when not in work and indentation of 3
+;; when in work.
+(if (eq system-type 'windows-nt)
+    (setq c-default-style "linux" c-basic-offset 3)
+  ((require 'google-c-style)
+   (add-hook 'c-mode-common-hook 'google-set-c-style)))
 
 ;;;; Python.
 ;; Default indentation: 4 for home (Mac) and we use 2 in work.
@@ -103,13 +115,15 @@
 (add-hook 'octave-mode-hook
           (lambda ()
             (abbrev-mode 1)
-            (auto-fill-mode 1)
+            (auto-fill-mode 0)
             (if (eq window-system 'x)
                 (font-lock-mode 1))))
 (setq octave-comment-start "%")
+(setq octave-continuation-string "...")
+(setq octave-block-offset 3)
 
 
-;;;; YAML mode
+;;;; YAML mode.
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
@@ -119,6 +133,14 @@
 (add-hook 'yaml-mode-hook
           '(lambda ()
              (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+;;;; Perl mode.
+(require 'perl-mode)
+(setq perl-indent-level 3)
+(add-to-list 'auto-mode-alist '("\\.pl$" . perl-mode))
+(add-to-list 'auto-mode-alist '("\\.tune$" . perl-mode))
+(add-to-list 'auto-mode-alist '("\\.symfun$" . perl-mode))
+
 
 ;;;; Backups.
 (setq
@@ -131,9 +153,25 @@
  version-control t)
 
 ;;;; Org mode.
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
 
 ;;;; Tags.
 (require 'etags-select)
 (global-set-key (kbd "M-.")'etags-select-find-tag)
 (global-set-key (kbd "M-?")'etags-select-find-tag-at-point)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "1a85b8ade3d7cf76897b338ff3b20409cb5a5fbed4e45c6f38c98eee7b025ad4" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
