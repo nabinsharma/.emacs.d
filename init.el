4d8a566671ffc60d3872aa6dfb652ff020c78e89
Nabin Sharma <nabin.s.sharma@gmail.com>
2016-01-21 11:48:35 -0500

    Couple of fixes and nits to run within cygwin

--- init.el
+++ init.el
@@ -14,7 +14,10 @@
 (setq default-directory "~")
 
 ;;;; Theme.
-(load-theme 'hickey t)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
+(if (eq system-type 'windows-nt)
+    (load-theme 'hickey t)
+  (load-theme 'solarized t))
 
 ;;;; Load required path and start server.
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp"))
@@ -49,7 +52,8 @@
 
 ;;;; Appearence.
 ;; Font.
-(set-face-attribute 'default nil :font "Inconsolata Medium")
+(if (eq system-type 'windows-nt)
+    (set-face-attribute 'default nil :font "Inconsolata Medium"))
 ;; Default indentation of 2 spaces.
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
@@ -71,13 +75,6 @@
 ;; Remove scrollbar.
 (if (fboundp 'scroll-bar-mode)(scroll-bar-mode -1))
 
-;;;; Cygwin shell
-(defun cygwin-shell ()
-  "Run cygwin bash in shell mode."
-  (interactive)
-  (let ((explicit-shell-file-name "C:/cygwin/bin/bash"))
-    (call-interactively 'shell)))
-
 ;;;; Grin.
 (require 'grin)
 
@@ -94,7 +91,7 @@
 (add-to-list 'auto-mode-alist '("\\.ipp$" . c++-mode))
 ;; Google C style when not in work and indentation of 3
 ;; when in work.
-(if (eq system-type 'windows-nt)
+(if (or (eq system-type 'windows-nt) (eq system-type 'cygwin))
     (setq c-default-style "linux" c-basic-offset 3)
   ((require 'google-c-style)
    (add-hook 'c-mode-common-hook 'google-set-c-style)))
@@ -165,16 +162,3 @@
 (require 'etags-select)
 (global-set-key (kbd "M-.")'etags-select-find-tag)
 (global-set-key (kbd "M-?")'etags-select-find-tag-at-point)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "1a85b8ade3d7cf76897b338ff3b20409cb5a5fbed4e45c6f38c98eee7b025ad4" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
